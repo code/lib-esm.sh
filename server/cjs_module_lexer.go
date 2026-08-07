d1695bca65c9e209b265221858ff9c68b822bb31
Je Xia <i@jex.me>
2026-08-07 20:39:38 +0800

    Update cjs_module_lexer version to 1.1.0

--- server/cjs_module_lexer.go
+++ server/cjs_module_lexer.go
@@ -25,7 +25,7 @@ import (
 	"github.com/ije/gox/utils"
 )
 
-var cjsModuleLexerVersion = "1.0.8"
+var cjsModuleLexerVersion = "1.1.0"
 var cjsModuleLexerIgnoredPackages = set.New(
 	"@babel/types",
 	"cheerio",
